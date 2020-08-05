2290154f6a37ff9b9499cec4c771e91f637db17e
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-08-05 11:48:38 +0530

    Added Program Description

---
+++ Images.js
@@ -0,0 +1,6 @@
+//This is a code for image insertion, setting height and width of the image in Java Script
+
+var img = new SimpleImage("PSX_20200313_144315.jpg");
+print(img.getWidth());
+print(img.getHeight());
+print(img);
